a3a4824a96bcdf8bcd1b7805c1fae3bd44a9180b
artem_ <artem_5511@mail.ru>
2023-04-18 23:15:09 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Accordion} from './components/Accordion/Accordion';
+import {Rating} from './components/Rating/Rating';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//function declaration
+function App(props: any) {
+    console.log("App rendering")
+    //полезное что то
+    //обязана вернуть JSX
+    return (
+        <div className="App">
+            {/*<PageTitle title={"This is APP component"}/>*/}
+            <Accordion titleValue={"Menu"} collapsed={true}/>
+            <Accordion titleValue={"Users"} collapsed={true}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
 
+type PageTitlePropsTitle = {
+    title: string
+}
+ function PageTitle (props: PageTitlePropsTitle) {
+     console.log("AppTitle rendering")
+return <h1>{props.title}</h1>
+}
 export default App;
